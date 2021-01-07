0cb9f68a5f82099090373ab3b2fdbc2645e7f54b
C3M3Z4 <cmeza@mezadigital.com>
2021-01-06 19:19:16 -0600

    [BEGINNING] Initial commit for this app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Esta la pagina inicial
+      </h1>
+      
+    </div>
+  );
+}
+
+export default App;
